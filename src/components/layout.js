61a120591dead762233493de1af73953a93746f8
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-04-02 19:23:29 -0600

    setting up

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children, location }) => {
       render={data => (
         <>
           <Helmet
-            title={data.site.siteMetadata.title}
+            title="Ammon Matthews"
             meta={[
               { name: 'description', content: 'Sample' },
               { name: 'keywords', content: 'sample, something' },
